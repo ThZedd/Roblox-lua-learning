9895b9e00a2fdc6b125a7d654352a78174b8f7bc
Bernardo Carvalho <122268749+ThZedd@users.noreply.github.com>
2023-08-27 11:53:06 +0100

    Add files via upload

---
+++ Roblox lua learning.lua
@@ -0,0 +1,222 @@
+--Printing
+
+print("Hello world!")
+
+
+--Variables
+
+local myName = 4 --local makes the script runs faster
+
+print(myName)
+
+-- Arithmetic + Object-Oriented Programming
+
+-- * - multiplication
+-- / - division
+-- + - addition
+-- - subtration 
+-- true or false its a boolean
+-- ^ exponentiation
+
+local PartTransparency = 0.3 + 0.3
+
+local PartMaterial = "Wood"
+
+local Baseplate = game.Workspace.Baseplate-- to make easier to script takes less time (instead of always writing game.workspace.baseplate)
+
+Baseplate.Transparency = PartTransparency -- it manipulates the transparency of the baseplate with basic scripting
+
+Baseplate.Material = PartMaterial  -- it manupulates the material of the baseplate with basic scripting
+
+--Functions
+
+local function MyFirstFunction()
+	local BestNumber = 15
+	local FavoriteNumber = 10
+	print(BestNumber+FavoriteNumber)
+end
+
+MyFirstFunction() -- calls the function to make it work, if we don´t call the function she will not be triggered and it will not work
+
+
+-- Scope
+local a = 2 -- if the variable is outside the fucntion, the variable is global and can be used by any function or command because is outside of the function
+
+local function Test1()
+	local b = 4 -- but if the variable is inside the function and we use local, the variable will be local and it only be used inside this function
+	c = 6
+	print(a)
+	print(b)
+	print(c)
+end
+
+local function Test2 ()
+	print(a)
+	print(b) -- this will not be printed cus the variable b is a local variable of the function Test1 and only works in that function
+	print(c) -- this will work cus the variable c is not local so it will run in another functions	
+end
+
+Test1()
+Test2()
+
+
+-- Returning
+
+local function hi()
+	print("hi")
+	return 0
+end
+
+local Hotdog = hi() -- we can make a variable equal to a function to get the value of the function and we be able to see the returning the value
+print(Hotdog)
+
+-- Parameters
+local function add(Number1, Number2) -- Number1 and Number2 are parameters that we can give values when we call the function they are basically variables
+	print(Number1+Number2)
+end
+
+add(3,2)
+
+local function adi(Hotdog, Ham) -- Number1 and Number2 are parameters that we can give values when we call the function they are basically variables
+	print(Hotdog)
+end
+
+adi("Sweet",2)-- Hotdog will be = "Sweet" and Ham will be = 2
+adi("Cool",2)-- Hotdog will be = "Cool" and Ham will still be = 2
+
+
+-- Else and Elseif Statements
+local DogWater = 3
+
+if DogWater == 2 then
+	print("DogWater is equal to 3")
+elseif DogWater == 3 then
+	print("DogWater isn´t equal to 3")
+else
+	print("None are equal to 3")
+end
+
+
+local Baseplate = game.Workspace.Baseplate
+
+if Baseplate.Anchored == true then
+	print("The baseplate is anchored")
+	Baseplate.Anchored = false
+	wait(2)
+	Baseplate.Anchored = true
+	return 0
+end
+
+-- Events
+
+game.Workspace.MyPart.Touched:Connect(function() -- this event works if the block MyPart is touched
+	print("I was stepped on")
+end)
+
+local function AwesomeSauce()
+	print("FUNCTION CALLED")
+	print("I WAS STEPPED ON")
+	game.Workspace.MyPart.Anchored = false
+end
+
+game.Workspace.MyPart.Touched:Connect(AwesomeSauce) 
+
+-- Built- in fucntions
+
+local Mylove = game.Workspace:WaitForChild("MyPart") -- it will wait for the part Mypart to exist to print "Yay my love "
+print("Yay my love")
+
+local MelonFriend = "MyPart"
+
+local Melon = game.Workspace:FindFirstChild(MelonFriend)
+
+-- While and Repeat Loops
+
+local doghot = 1
+
+while doghot < 5 do -- it will only run if doghot < 5 (it doesn´t run automatically)
+	print("Hello")
+	print(doghot)
+	doghot = doghot + 1
+end
+
+repeat -- automatically will run until something happens
+	print("Hello")
+	doghot = doghot + 1
+until doghot == 3 
+
+
+-- More events
+
+local MyPart = game.Workspace.MyPart
+
+MyPart.Touched:Connect(function(hit)
+	print(hit) -- this will print the part that touched the block
+	if hit.Parent:FindFirstChild("Humanoid") then -- this will find my character or the humanoid so if anything else touches it, it will not bug then
+		hit.Parent.Humanoid.Health = 0
+		
+	end
+end)
+
+
+-- Instances
+local NewPart = Instance.new("Part", game.Workspace) -- it will create a part while the game is running after that the part is deleted
+
+NewPart.Size = Vector3.new(50,50,50) -- vector3 is a 3 value (x,y,z) we use vector3 to all the 3 values variables
+NewPart.Position = Vector3.new(50,200,50)
+NewPart.Anchored = true
+
+-- Making rain block
+
+
+while true do
+	wait()
+	local rain = Instance.new("Part", game.Workspace) -- this will create a new part forever
+	rain.Position = Vector3.new(0,50,0)
+	rain.Size = Vector3.new(0.2,0.5,0.2)
+	rain.Anchored = false
+	rain.Transparency = 0.25
+	rain.BrickColor = BrickColor.new("Baby blue")
+
+end
+
+
+-- Breaks / Loop Breaking
+
+local rainspawned = 0
+
+while true do
+	if rainspawned >= 250 then
+		break
+	end
+	rainspawned = rainspawned + 1
+	wait()
+	local rainSpawner = Instance.new("Part",game.Workspace)
+	rainSpawner.Size = Vector3(0.5,2,0.5)
+	rainSpawner.Position = Vector3(0,50,0)
+	rainSpawner.Anchored = false
+	rainSpawner.Transparency = 0.5
+end
+
+-- Random
+
+local RandomNumber = math.random(1,30) -- this will get a random number between 1 and 30
+
+math.randomseed(tick()) -- tick is the time since 1967, so if we want to always have a random seed we put tick there cus is always different
+
+-- Random Rain
+
+
+while true do
+	if rainspawned >= 250 then
+		break
+	end
+	local randomrain = math.random(1,53)
+	rainspawned = rainspawned + 1
+	wait()
+	local rainSpawner = Instance.new("Part",game.Workspace)
+	rainSpawner.Size = Vector3(0.5,2,0.5)
+	rainSpawner.Position = Vector3(randomrain,50,randomrain)
+	rainSpawner.Anchored = false
+	rainSpawner.Transparency = 0.5
+end
